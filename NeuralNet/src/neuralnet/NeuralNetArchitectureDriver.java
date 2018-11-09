cc2dfd4d96e59a8a192adbb0f488847041c94b77
Wesley Byrne <wbyrne526@gmail.com>
2018-11-09 09:22:47 -0600

    Final update, and completed architecture computations

--- NeuralNet/src/neuralnet/NeuralNetArchitectureDriver.java
+++ NeuralNet/src/neuralnet/NeuralNetArchitectureDriver.java
@@ -41,55 +41,27 @@ public class NeuralNetArchitectureDriver {
 		
         ArrayList<int[]> combos=new ArrayList<int[]>();
         int[] combo;
-		for (int l=1;l<=max_hidden_layers;l++)
+		for (int l=1; l<=max_hidden_layers;l++)
 		{
-            
-            combo = new int[l+2];
-            combo[0] = learner.getInputSize();
-            combo[(combo.length)-1] = learner.getOutputSize();
-            
-			for (int i=l;i>=1;i--)
-			{
-                recursiveFill(combos, combo, i, max_nodes);
-			}				
-        }
+			combo = new int[l+2];
+			combo[0] = learner.getInputSize();
+			combo[(combo.length)-1] = learner.getOutputSize();
+			recursiveFill(combos, combo, 1, l, max_nodes);
+		}				
         
-        for (int[] ints:combos){
-            /*for (int num:ints) {
-                System.out.print(num);
-            }*/
+        for (int[] ints:combos)
             nets.add(new NeuralNet(ints,learning_rate,error_tol,learner.getLargestVal()));
-            //System.out.print(", ");
-        }
-        System.out.print("\nCombos to process:"+combos.size()+"\n");
-        
-        /*NeuralNet new_net;
-        int[] nodes;
-        
-        int node_size=1;
-
-        for (int i=1;i<=max_nodes;i++)
-        {
-            for(int j=1;j<=max_hidden_layers;j++)
-            {
-                nodes=new int[j+2];
-                nodes[0] = learner.getInputSize();
-                nodes[(nodes.length)-1] = learner.getOutputSize();
-                for (int k=1;k<nodes.length-1;k++)
-                {
-                    nodes[k] = i;
-                }
-                new_net = new NeuralNet(nodes, learning_rate, error_tol, learner.getLargestVal());
-                nets.add(new_net);
-            }
-        }*/
-        
+		
+		System.out.println("Combos to process:"+combos.size());
+		
         //10 fold cross validation
         ArrayList<Double> errors;
         ArrayList<DataPoint> examples;
-        for (NeuralNet net: nets)
+		NeuralNet best_net = nets.get(0);
+		java.util.Iterator<NeuralNet> it = nets.iterator();
+        while (it.hasNext())
         {
-            
+            NeuralNet net = it.next();
             net.reportArchitecture();
             errors = new ArrayList<Double>();
             examples = new ArrayList<DataPoint>();
@@ -99,20 +71,19 @@ public class NeuralNetArchitectureDriver {
                     if (j!=i) examples.addAll(k_bins.get(j));
 				System.out.println("processing...");
                 net = learner.BackPropAlg(net, examples);
-                examples = net.returnOutput(k_bins.get(i)); 
-                errors.add(SumOutputs.sum(learner.getLargestVal(), examples));
-                net.avg_err = SumOutputs.avg_err(errors);
-                
+				examples = net.returnOutput(k_bins.get(i)); 
+				errors.add(SumOutputs.sum(learner.getLargestVal(), examples));
             }
+			net.avg_err = SumOutputs.avg_err(errors);
+			if (net.avg_err < best_net.avg_err ){
+				System.out.println(net.avg_err+"<"+best_net.avg_err);	
+				best_net = net;
+			}
+			it.remove();
         }
 
-		//cycle through the networks to see find the smallest average error
-        NeuralNet best_net = nets.get(0);
-        for (NeuralNet net:nets)
-            if (net.avg_err < best_net.avg_err )
-                best_net = net;
         
-        System.out.println("\nBest Architecture:");
+        System.out.println("\nBest Found Architecture:");
         best_net.reportArchitecture();
         System.out.println("err: " +best_net.avg_err);
 
@@ -132,9 +103,9 @@ public class NeuralNetArchitectureDriver {
 
     }
 	
-	private static void recursiveFill(ArrayList combos, int[] combo, int i, int max)
+	private static void recursiveFill(ArrayList combos, int[] combo, int i, int num_layers, int max)
 	{
-        if(i==0)
+        if(i==num_layers+1)
         {
             combos.add(combo.clone());
             return;
@@ -142,7 +113,7 @@ public class NeuralNetArchitectureDriver {
 		for(int j=1;j<=max;j++)
         {
             combo[i] = j;
-            recursiveFill(combos, combo, i-1, max);
+            recursiveFill(combos, combo, i+1, num_layers, max);
         }
     }
 }
